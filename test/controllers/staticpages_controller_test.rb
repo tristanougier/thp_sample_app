a2743f608b4753eaf689ec347f7c5dc4d311a641
Tristan Ougier <tristanougier@gmail.com>
2018-01-31 15:13:51 +0100

    finish static_pages

--- test/controllers/staticpages_controller_test.rb
+++ test/controllers/staticpages_controller_test.rb
@@ -6,10 +6,10 @@ class StaticpagesControllerTest < ActionDispatch::IntegrationTest
 		@base_title = "Ruby on Rails Tutorial Sample App"
 	end
 
-  test "should get root"
+  test "should get root" do
   	get staticpages_home_url
   	assert_response :success
-
+  end
 
   test "should get home" do
     get staticpages_home_url 
